bb8dcbe92d4105d29b2838b4755a66a39d9e510d
civ2018 <zhuangxusc@gmail.com>
2022-06-03 14:48:59 +0800

    Update plainnet_20_56_train_loss.py

--- Classification/draw_figure/plainnet_20_56_train_loss.py
+++ Classification/draw_figure/plainnet_20_56_train_loss.py
@@ -1,9 +1,11 @@
+import os
 import torch
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    plainnet20_train_loss = torch.load("../../output/PlainNet_20-Layers_CIFAR10/last.pth")["trn_loss"]
-    plainnet56_train_loss = torch.load("../../output/PlainNet_56-Layers_CIFAR10/last.pth")["trn_loss"]
+    print(f"The current path is: {os.getcwd()}")
+    plainnet20_train_loss = torch.load("Output/PlainNet_20-Layers_CIFAR10/last.pth")["trn_loss"]
+    plainnet56_train_loss = torch.load("Output/PlainNet_56-Layers_CIFAR10/last.pth")["trn_loss"]
     plt.figure(figsize=(12, 8), dpi=80)
     epoch_list = [i + 1 for i in range(len(plainnet20_train_loss))]
     plt.plot(epoch_list, plainnet20_train_loss, color="red", label="training_loss_20")
@@ -11,5 +13,8 @@ if __name__ == "__main__":
     plt.xlabel(f"iterations x500")
     plt.ylabel("training loss")
     plt.legend(loc="upper right")
-    plt.savefig("../../output/ResNet/PlainNet_train_loss.jpg")
+
+    if not os.path.exists("Output/ResNet_Figures"):
+        os.makedirs("Output/ResNet_Figures")
+    plt.savefig("Output/ResNet_Figures/PlainNet_train_loss.jpg")
     plt.close()
