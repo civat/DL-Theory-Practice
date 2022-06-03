659d3032c30d61cdc7793961892c9ccf610244f4
civ2018 <zhuangxusc@gmail.com>
2022-06-03 14:48:40 +0800

    Update plainnet_20_56_gradients.py

--- Classification/draw_figure/plainnet_20_56_gradients.py
+++ Classification/draw_figure/plainnet_20_56_gradients.py
@@ -1,9 +1,11 @@
+import os
 import torch
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    plainnet20_state = torch.load("../../output/PlainNet_20-Layers_CIFAR10/last.pth")["gradients"]
-    plainnet56_state = torch.load("../../output/PlainNet_56-Layers_CIFAR10/last.pth")["gradients"]
+    print(f"The current path is: {os.getcwd()}")
+    plainnet20_state = torch.load("Output/PlainNet_20-Layers_CIFAR10/last.pth")["gradients"]
+    plainnet56_state = torch.load("Output/PlainNet_56-Layers_CIFAR10/last.pth")["gradients"]
 
     # Change the layer number to show gradients of different layers.
     layer = "convs.0"
@@ -17,5 +19,8 @@ if __name__ == "__main__":
     plt.xlabel(f"iterations x500")
     plt.ylabel("gradients")
     plt.legend(loc="upper right")
-    plt.savefig(f"../../output/ResNet/PlainNet_train_gradient_{layer}.jpg")
+
+    if not os.path.exists("Output/ResNet_Figures"):
+        os.makedirs("Output/ResNet_Figures")
+    plt.savefig(f"Output/ResNet_Figures/PlainNet_train_gradient_{layer}.jpg")
     plt.close()
