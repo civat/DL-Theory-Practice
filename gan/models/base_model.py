0db9238330eb5088eb667f56b5c50af5de58534d
zhuangxu <zhuangxusc@gmail.com>
2022-12-25 11:10:25 +0800

    Feat: reformat

--- gan/models/base_model.py
+++ gan/models/base_model.py
@@ -24,8 +24,8 @@ class BaseModel(ABC):
             -- self.visual_names (str list):        specify the images that you want to display and save.
             -- self.optimizers (optimizer list):    define and initialize optimizers. You can define one optimizer for each network. If two networks are updated at the same time, you can use itertools.chain to group them. See cycle_gan_model.py for an example.
 
-        Parameters:
-        -----------
+        Parameters
+        ----------
         configs: dict
           All configs of the model.
         """
