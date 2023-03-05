93acc50cd1b61da9fe8eed418646b950131c5fc4
zhuangxu <zhuangxusc@gmail.com>
2023-03-05 17:24:54 +0800

    update GAN model

--- gan/models/gan.py
+++ gan/models/gan.py
@@ -1,11 +1,13 @@
 import os
 import torch
+import torch.nn as nn
 import matplotlib.pyplot as plt
 from ptflops import get_model_complexity_info
 
 import register
 from gan.gan_loss import GANLoss
 from gan.models.base_model import BaseModel
+from gan.gan_loss import cal_gradient_penalty
 from classification import utils
 
 
@@ -25,39 +27,24 @@ class GAN(BaseModel):
         # "D" means "Discriminator".
         self.model_names = ["G", "D"]
 
-        # Infer the device used for training.
-        # The simplest way to set the value is:
-        # 1) set to "cpu" if cpu is used;
-        # 2) set to a list of IDs (int) if GPUs are used.
-        # E.g. you can set to [0] is only one GPU is used.
-        device = configs["Train"]["device"]
-        device_id = None
-        if isinstance(device, str):
-            assert device in ["cuda", "cpu"]
-            if device == "cuda":
-                device_id = "cuda:0"
-                device_ids = [0]
-        elif isinstance(device, list):
-            device_id = f"cuda:{device[0]}"
-            device_ids = list(device)
-            device = "cuda"
-        self.device_id = device_id
-        self.device = device
-
         # Define generator and discriminator
         self.G, self.G_configs = register.make_network(configs["G"])
         self.D, self.D_configs = register.make_network(configs["D"])
 
-        # Set multi-GPU mode if more than one GPU used.
-        if device == "cuda":
-            self.G = torch.nn.DataParallel(self.G, device_ids=device_ids)
-            self.G = self.G.to(device_id)
-            self.D = torch.nn.DataParallel(self.D, device_ids=device_ids)
-            self.D = self.D.to(device_id)
+        # Infer the device used for training
+        self.device_id, self.device_ids, self.device = utils.parse_device(configs["Train"]["device"])
 
         # Define the loss function
-        self.gan_loss = GANLoss(configs["Train"]["GAN_loss"])
-        self.gan_loss.to(device)
+        self.gan_loss_name = GANLoss(configs["Train"]["GAN_loss"])
+        if isinstance(self.gan_loss_name, dict):
+            self.gan_loss_name = list(self.gan_loss_name.keys())
+            if len(self.gan_loss_name) != 1:
+                raise Exception("Only one loss can (must) be specified!")
+            self.gan_loss_name = self.gan_loss_name[0]
+            self.gan_loss_args = configs["Train"]["GAN_loss"][self.gan_loss_name]
+
+        self.gan_loss = GANLoss(self.gan_loss_name)
+        self.gan_loss.to(self.device_id)
 
         # Define optimizers
         self.optimizer_G = utils.get_optimizer(self.G.parameters(), configs["G"]["OPT"])
@@ -70,7 +57,13 @@ class GAN(BaseModel):
         if "Scheduler" in configs["D"]:
             self.scheduler_D = utils.get_scheduler(self.optimizer_D, configs["D"]["Scheduler"])
 
+        if "Init" in configs["G"]:
+            utils.init_nn(self.G, configs["G"]["Init"])
+        if "Init" in configs["D"]:
+            utils.init_nn(self.D, configs["D"]["Init"])
+
         self.n_save_imgs = configs["Train"]["save_imgs"]  # number of generated images to save
+        self.n_critics = configs["Train"]["n_critics"] if "n_critics" in configs["Train"] else 1
 
         # used for drawing figures
         self.iterations = 1
@@ -108,7 +101,10 @@ class GAN(BaseModel):
         loss_D_fake = self.gan_loss(pred_fake, False)
 
         # Combined loss and calculate gradients
-        self.loss_D_A = (loss_D_real + loss_D_fake) * 0.5
+        self.loss_D_A = loss_D_real + loss_D_fake
+        if self.gan_loss_name == "wgangp":
+            gradient_penalty = cal_gradient_penalty(self.D, self.imgs_real, self.imgs_fake.detach(), self.device_id, **self.gan_loss_args)
+            self.loss_D_A += gradient_penalty
         self.loss_D_A.backward()
 
     def backward_G(self):
@@ -131,10 +127,11 @@ class GAN(BaseModel):
         self.optimizer_D.step()                  # update D's weights
 
         # G
-        self.set_requires_grad([self.D], False)  # D require no gradients when optimizing G
-        self.optimizer_G.zero_grad()             # set G's gradients to zero
-        self.backward_G()                        # calculate gradients for G
-        self.optimizer_G.step()                  # update G's weights
+        if self.iterations % self.n_critics == 0:
+            self.set_requires_grad([self.D], False)  # D require no gradients when optimizing G
+            self.optimizer_G.zero_grad()             # set G's gradients to zero
+            self.backward_G()                        # calculate gradients for G
+            self.optimizer_G.step()                  # update G's weights
 
         if self.scheduler_G is not None:
             self.scheduler_G.step()
@@ -153,7 +150,7 @@ class GAN(BaseModel):
         imgs = []
         with torch.no_grad():
             for data in data_loader:
-                vecs_rand = data["vec"].to(self.device)            
+                vecs_rand = data["vec"].to(self.device_id)
                 fake = self.G(vecs_rand)
                 imgs.append((f"{count}_fak", fake))
                 count += 1
@@ -164,15 +161,23 @@ class GAN(BaseModel):
     def setup(self, configs, log):
         # Calculate the model compexity.
         # The current method is based on the ptflops.
-        # But I found that it cannot get correct results when multi-GPUs are used.
+        # ptflops cannot get correct results when multi-gpus are used.
+        # So we first move models to only one GPU.
+        self.G = self.G.to(self.device_id)
+        self.D = self.D.to(self.device_id)
+
+        log.logger.info("Model complexity of Generator:")
         input_shape = (configs["Dataset"]["vec_dim"],)
-        macs, params = get_model_complexity_info(self.G,
-                                                 input_shape,
-                                                 as_strings=True,
-                                                 print_per_layer_stat=True,
-                                                 verbose=True)
-        log.logger.info('{:<30}  {:<8}'.format('Computational complexity: ', macs))
-        log.logger.info('{:<30}  {:<8}'.format('Number of parameters    : ', params))
+        utils.cal_model_complexity(self.G, input_shape, log)
+
+        log.logger.info("Model complexity of Discriminator:")
+        input_shape = (configs["Dataset"]["c"],
+                       configs["Dataset"]["h"],
+                       configs["Dataset"]["w"])
+        utils.cal_model_complexity(self.D, input_shape, log)
+
+        self.G = utils.set_device(self.G, self.device_id, self.device_ids, self.device)
+        self.D = utils.set_device(self.D, self.device_id, self.device_ids, self.device)
 
     def save_figures(self, output_path):
         plt.figure(figsize=(20, 8), dpi=80)
