c29fb2c9b467dd89ebac95ca2df6a5e1d3e42e49
zhuangxu <zhuangxusc@gmail.com>
2023-03-05 16:22:54 +0800

    Add gan_loss.py

--- gan/gan_loss.py
+++ gan/gan_loss.py
@@ -97,7 +97,7 @@ def cal_gradient_penalty(netD, real_data, fake_data, device, type='mixed', const
         else:
             raise NotImplementedError('{} not implemented'.format(type))
         interpolatesv.requires_grad_(True)
-        disc_interpolates = netD(interpolatesv)['patch_out']
+        disc_interpolates = netD(interpolatesv)
         gradients = torch.autograd.grad(outputs=disc_interpolates, inputs=interpolatesv,
                                         grad_outputs=torch.ones(disc_interpolates.size()).to(device),
                                         create_graph=True, retain_graph=True, only_inputs=True)
