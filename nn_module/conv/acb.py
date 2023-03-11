42d83147e7998fe0b670d6e933198679968bfbe2
zhuangxu <zhuangxusc@gmail.com>
2023-03-11 17:28:30 +0800

    code restructure

--- nn_module/conv/acb.py
+++ nn_module/conv/acb.py
@@ -4,7 +4,10 @@ import functools
 import torch.nn as nn
 import torch.nn.init as init
 
+import register
 
+
+@register.NAME_TO_CONVS.register("ACBlock")
 class ACBlock(nn.Module):
 
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1,
