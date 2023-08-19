740c49bcb59fa8ade8fae62c8d123725ac15a7b2
zhuangxu <zhuangxusc@gmail.com>
2023-08-19 23:27:55 +0800

    Add comment

--- nn_module/conv/convs.py
+++ nn_module/conv/convs.py
@@ -8,9 +8,37 @@ from classification import utils
 
 @register.NAME_TO_CONVS.register("Conv2d")
 class Conv2d(nn.Module):
+    """
+    The meta-build block of neural network.
+    This is a wrapper of `torch.nn.Conv2d`.
+    """
 
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding=0, dilation=1, groups=1, bias=True,
                  padding_mode="zeros", spectral_norm=False):
+        """
+        Parameters
+        ----------
+        in_channels : int
+            The number of input channels.
+        out_channels : int
+            The number of output channels.
+        kernel_size : int or tuple of int
+            The size of the kernel.
+        stride : int or tuple of int
+            The stride of the kernel.
+        padding : int or tuple of int
+            The padding of the input.
+        dilation : int or tuple of int
+            The dilation of the kernel.
+        groups : int
+            The number of groups.
+        bias : bool
+            Whether to use bias.
+        padding_mode : str
+            The padding mode.
+        spectral_norm : bool
+            Whether to use spectral normalization.
+        """
         super().__init__()
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride=stride, padding=padding, dilation=dilation,
                               groups=groups, bias=bias, padding_mode=padding_mode)
@@ -38,9 +66,37 @@ class Conv2d(nn.Module):
 
 @register.NAME_TO_CONVS.register("Conv2dTranspose")
 class Conv2dTranspose(nn.Module):
+    """
+    The meta-build block of neural network.
+    This is a wrapper of `torch.nn.ConvTranspose2d`.
+    """
 
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding=0, dilation=1, groups=1, bias=True,
                  padding_mode="zeros", spectral_norm=False):
+        """
+        Parameters
+        ----------
+        in_channels : int
+            The number of input channels.
+        out_channels : int
+            The number of output channels.
+        kernel_size : int or tuple of int
+            The size of the kernel.
+        stride : int or tuple of int
+            The stride of the kernel.
+        padding : int or tuple of int
+            The padding of the input.
+        dilation : int or tuple of int
+            The dilation of the kernel.
+        groups : int
+            The number of groups.
+        bias : bool
+            Whether to use bias.
+        padding_mode : str
+            The padding mode.
+        spectral_norm : bool
+            Whether to use spectral normalization.
+        """
         super().__init__()
         self.conv = nn.ConvTranspose2d(
             in_channels, out_channels, kernel_size, stride=stride, padding=padding, dilation=dilation,
@@ -69,9 +125,41 @@ class Conv2dTranspose(nn.Module):
 
 @register.NAME_TO_CONVS.register("Conv2dUp")
 class Conv2dUp(nn.Module):
+    """
+    The meta-build block of neural network.
+    This is an upsample block consisting of `torch.nn.Upsample` and `torch.nn.Conv2d`.
+    """
 
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding=0, dilation=1, groups=1, bias=True,
                  padding_mode="zeros", mode="nearest", spectral_norm=False):
+        """
+        Parameters
+        ----------
+        in_channels : int
+            The number of input channels.
+        out_channels : int
+            The number of output channels.
+        kernel_size : int or tuple of int
+            The size of the kernel.
+        stride : int or tuple of int
+            The stride of the kernel.
+        padding : int or tuple of int
+            The padding of the input.
+        dilation : int or tuple of int
+            The dilation of the kernel.
+        groups : int
+            The number of groups.
+        bias : bool
+            Whether to use bias.
+        padding_mode : str
+            The padding mode.
+        mode : str
+            The upsampling algorithm: one of ``'nearest'``,
+            ``'linear'``, ``'bilinear'``, ``'bicubic'`` and ``'trilinear'``.
+            Default: ``'nearest'``
+        spectral_norm : bool
+            Whether to use spectral normalization.
+        """
         super().__init__()
         self.up = nn.Upsample(scale_factor=stride, mode=mode)
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=padding, dilation=dilation,
