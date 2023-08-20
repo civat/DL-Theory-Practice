aa67f737fc2c8bffa92021f0b051fe41d555770a
zhuangxu <zhuangxusc@gmail.com>
2023-08-20 16:59:08 +0800

    Add conv_norm_act block

--- nn_module/conv/conv_norm_act.py
+++ nn_module/conv/conv_norm_act.py
@@ -9,6 +9,16 @@ from nn_module.norm.dispatch_norm import DispatchNorm
 
 @register.NAME_TO_CONVS.register("ConvNormAct")
 class ConvNormAct(nn.Module):
+    """
+    Building block that is generally used in most convolutional neural networks.
+    The ConvNormAct block is composed of one sub-block conv, one normalization layer and one activation layer.
+    Example1:
+        By setting conv to Conv2d, norm to BatchNorm2d and act to ReLU, the ConvNormAct block is simply a standard
+        conv-bn-relu block.
+    Example2:
+        By setting conv to ResBlock(nn_module/conv/res_block/ResBlock), norm to BatchNorm2d and act to ReLU,
+        the ConvNormAct block is a residual block defined in ResNet.
+    """
 
     def __init__(self, in_channels, out_channels, stride, norm=nn.BatchNorm2d, act=nn.ReLU, dropout=0.0, pre_act=False, conv=Conv2d):
         super().__init__()
